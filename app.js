a05bd8f324da25eb6bd37777ea2ca3c8bd53f399
Ashley Sulaiman <ashley.sulaiman83@gmail.com>
2021-07-02 12:53:07 -0400

    Completed Assignment

--- app.js
+++ app.js
@@ -3,5 +3,28 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) 
+{
+    for (let i = 1; i < count; i++)
+    {
+        if (i % 2 == 1) {
+            console.log(i);
+        } else {
+            continue;
+        }
+    }
+
+}
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+function checkAge(userName, age);{
+    let aboveSixteen = `"Congrats ${userName}, you can drive!"`;
+    let belowSixteen = `"Sorry ${username}, but you need to wait until you are 16."`;
+    if (age <=16) {
+        console.log(belowSixteen);
+    }
+    else {
+        console.log(aboveSixteen);
+    }
+    
+}
